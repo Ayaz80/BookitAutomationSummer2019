9b5040d78a4e4ea8a4e7c2659e6adaade20cc845
your_github_name <your_github_email>
2020-02-07 17:08:12 -0600

    CreateStudents

--- src/test/java/com/bookit/step_definitions/APIStepDefinitions.java
+++ src/test/java/com/bookit/step_definitions/APIStepDefinitions.java
@@ -1,6 +1,8 @@
 package com.bookit.step_definitions;
 
+import com.bookit.pojos.Room;
 import com.bookit.utilities.APIUtilities;
+import io.cucumber.java.bs.A;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
@@ -9,10 +11,13 @@ import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import org.junit.Assert;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
 import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
 
 
 public class APIStepDefinitions {
@@ -64,6 +69,14 @@ public class APIStepDefinitions {
     public void user_sends_POST_request_to_with_following_information(String path,List<Map<String,String>> students) {
        for(Map<String,String> student:students){
 
-       }
+           response=given().
+                   auth().oauth2(token).
+                   queryParams(student).
+                   accept(contentType).
+           when().
+                   post(path);
+
+           response.then().log().body(true);
+             }
     }
 }
